54b6f1d3355f9a50e7af46980fb56ca265349dba
Cindy Berghuizen <c.berghuizen@gmail.com>
2013-10-07 21:40:18 +0200

    started on report + exercise 7 + adding types

--- Week6/Lab6.hs
+++ Week6/Lab6.hs
@@ -55,38 +55,76 @@ jup = do
 composites :: [Integer]
 composites = filter (>0) (sieveF [-2,-3..])
 
+sieveF :: [Integer] -> [Integer]
 sieveF (p:xs) |(p>0) = p: sieveF xs
               |otherwise = p : (sieveF (map (markC p) xs))
               
+markC :: Integer -> Integer -> Integer              
 markC x y = if (rem y x /= 0) || (y>0) then y else -y
 
 -- EXERCISE 4
--- shows all the composite numbers it thinks is prime according to primeF
+-- shows all the composite numbers primeF checks as prime
+testF :: Int -> IO [Integer]
 testF k = 
     filterM (primeF k) (take 50 composites)
--- k = 1 = 4
--- k = 2 = 4 
--- k= 3 = 4
--- the higher k the less primes is finds (which is logical because there are no primes) 
--- it finds less primes because it takes more random a's to test is thorougly
+-- Run multiple tests
+testFMore :: Integer -> Int -> IO [[Integer]]  
 testFMore 0 _ = return []
 testFMore n k = do
     c <- testF k
     d <- testFMore (n-1) k
     return $ filter (not . null) (c:d)
-
+    
 -- EXERCISE 5
--- Carmichael almost always passes the test so logically we see a lot more numbers passing the test
+testCar :: Int -> IO [Integer]
 testCar k =
     filterM (primeF k) (take 50 carmichael)
-    
--- EXERCISE 6
-testMR k =
-    filterM (primeMR k) (take 50 carmichael)
-    
-testMore 0 _ = return []
-testMore n k = do
-    c <- testMR k
-    d <- testMore (n-1) k
+
+testMore1 :: Integer -> Int -> IO [[Integer]]    
+testMore1 0 _ = return []
+testMore1 n k = do
+    c <- testCar k
+    d <- testMore1 (n-1) k
     return $ filter (not . null) (c:d)
     
+-- EXERCISE 6
+testMR :: Int -> IO [Integer]
+testMR k =
+    filterM (primeMR k) (take 50 carmichael)
+
+test2More :: Integer -> Int -> IO [[Integer]]    
+test2More 0 _ = return []
+test2More n k = do
+    c <- testMR k
+    d <- test2More (n-1) k
+    return $ filter (not . null) (c:d)
+
+-- functions to show the difference in result of Fermat's primality and Miller-Rabin's primality check
+lengthCar :: Integer -> Int ->  IO String   
+lengthCar n k= do
+    f <- testMore1 n k
+    return $ show (length f)
+
+lengthMR :: Integer -> Int ->  IO String   
+lengthMR n k = do
+    f <- test2More n k
+    return $ show (length f)
+    
+-- EXERCISE 7
+--take a large prime, use miller rabin to check if 2^p -1 ook prime is (dan is het een mersenne getal)
+multipleMersenne :: Integer -> Int -> IO [(Bool, Integer, Integer)]
+multipleMersenne 0 _ = return []
+multipleMersenne n k = do
+    m <- mersenne k
+    c <- multipleMersenne (n-1) k
+    return $ (m : c)
+
+mersenne :: Int -> IO (Bool, Integer, Integer)
+mersenne k = do
+    p <- randomPrime
+    m <- primeMR k ((2^p) - 1)
+    return $ (m,p,((2^p) - 1)) -- 
+
+randomPrime = do
+    b <- (randomRIO (1,100)) 
+    return (primes !! b)
